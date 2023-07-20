8bef68ffb0148762d395df002b780fd3c89cdb74
Naufal Fachrian <naufal.fachrian@gmail.com>
2023-07-20 17:25:46 +0700

    Implement rest routes

--- Sources/VTuberSheet/routes.swift
+++ Sources/VTuberSheet/routes.swift
@@ -2,4 +2,16 @@ import Vapor
 
 func routes(_ app: Application) throws {
 
+    app.get("vtubers") { req in 
+        return try await app.vtuberSheet.vtubers
+            .paginate(for: req)
+            .encode(for: req)
+    }
+
+    app.get("affiliations") { req in
+        return try await app.vtuberSheet.affiliations
+            .paginate(for: req)
+            .encode(for: req)
+    }
+
 }
